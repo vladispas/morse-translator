834e5ce0bd8cf7b2bfbb3c721f33920dfbaffdd7
Vlad Ispas <vlad.ispas@yahoo.com>
2024-07-03 15:40:27 +0300

    Project setup.

--- MorseTranslator/MorseTranslatorApp.swift
+++ MorseTranslator/MorseTranslatorApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct MorseTranslatorApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
+    
 }
