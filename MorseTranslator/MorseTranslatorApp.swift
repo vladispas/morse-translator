980b7939df6353d44e1b1462ad3318b63249d6bb
Vlad Ispas <vlad.ispas@yahoo.com>
2024-07-03 15:38:59 +0300

    Created Xcode project.

---
+++ MorseTranslator/MorseTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseTranslatorApp.swift
+//  MorseTranslator
+//
+//  Created by Vlad Ispas on 03.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MorseTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
